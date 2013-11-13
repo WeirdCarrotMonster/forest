1ce66f5827a5035f05618476eb6140d7866d70c8
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2013-11-13 11:23:36 +0400

    PEP8

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from components.trunk import Trunk
 from components.roots import Roots
 from components.branch import Branch
 from components.air import Air, get_leaves_proxy
-from components.common import CommonListener, TransparentListener, WebSocketListener, log_message
+from components.common import CommonListener, TransparentListener, \
+    WebSocketListener, log_message
 
 if len(sys.argv) < 2:
     log_message(
@@ -24,61 +25,65 @@ if len(sys.argv) < 2:
     )
     sys.exit(0)
 
-file = sys.argv[1]
+FILENAME = sys.argv[1]
 FOREST_DIR = os.path.dirname(os.path.realpath(__file__))
 PID_DIR = os.path.join(FOREST_DIR, 'pid')
 
-json_data = open(os.path.join(FOREST_DIR, file))
-SETTINGS = json.load(json_data)
-json_data.close()
+JSON_DATA = open(os.path.join(FOREST_DIR, FILENAME))
+SETTINGS = json.load(JSON_DATA)
+JSON_DATA.close()
 
 if len(sys.argv) == 3 and sys.argv[2] == "shell_config":
     get_leaves_proxy(SETTINGS)
     sys.exit(0)
 
 # Определяем слушателей по ролям
-listeners = []
-settings = SETTINGS["settings"]
+LISTENERS = []
 
 if not SETTINGS["role"] in ["roots", "trunk", "branch", "air"]:
-    log_message("Configuration error: unknown role '{0}'".format(SETTINGS["role"]))
+    log_message(
+        "Configuration error: unknown role '{0}'".format(SETTINGS["role"])
+        )
     sys.exit(0)
 
-application = None
+APPLICATION = None
 log_message("Setting role: {0}".format(SETTINGS["role"]))
 
 if SETTINGS["role"] == "roots":
-    listeners.append((r"/", CommonListener))
-    application = Roots(settings, handlers=listeners)
+    LISTENERS.append((r"/", CommonListener))
+    APPLICATION = Roots(SETTINGS["settings"], handlers=LISTENERS)
 
 if SETTINGS["role"] == "trunk":
-    listeners.append((r"/", TransparentListener))
-    listeners.append((r"/websocket", WebSocketListener))
-    application = Trunk(settings, handlers=listeners)
+    LISTENERS.append((r"/", TransparentListener))
+    LISTENERS.append((r"/websocket", WebSocketListener))
+    APPLICATION = Trunk(SETTINGS["settings"], handlers=LISTENERS)
 
 if SETTINGS["role"] == "branch":
-    listeners.append((r"/", CommonListener))
-    application = Branch(settings, handlers=listeners)
+    LISTENERS.append((r"/", CommonListener))
+    APPLICATION = Branch(SETTINGS["settings"], handlers=LISTENERS)
 
 if SETTINGS["role"] == "air":
-    listeners.append((r"/", CommonListener))
-    application = Air(settings, handlers=listeners)
+    LISTENERS.append((r"/", CommonListener))
+    APPLICATION = Air(SETTINGS["settings"], handlers=LISTENERS)
 
 # Создаем и запускаем приложение
 log_message("Listening on: {0}:{1}".format(
     SETTINGS["connections"]["address"], 
     SETTINGS["connections"]["port"])
 )
-application.listen(SETTINGS["connections"]["port"], SETTINGS["connections"]["address"])
+APPLICATION.listen(
+    SETTINGS["connections"]["port"], 
+    SETTINGS["connections"]["address"]
+    )
 
 
 def cleanup(signum=None, frame=None):
     if signum:
-        log_message("Got signum: {0}".format(signum))
+        log_message("Got signum: {0}, frame {1}".format(signum, frame))
     try:
         log_message("Cleaning up...")
-        application.shutdown_leaves()
-    except:
+        APPLICATION.shutdown_leaves()
+    except AttributeError:
         pass
     finally:
         log_message("Done!")
