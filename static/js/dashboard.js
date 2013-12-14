5c5aae1ebb60f26c7ca675b7bb1c6a29e65bbd6e
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2013-12-14 13:27:32 +0400

    Микропрокачка

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,6 +1,13 @@
 function Dashboard($scope, $http, $timeout) {
     $scope.servers = [];
 
+    $scope.getNiceLookingPercent = function(part, all){
+        if (all == 0){
+            all = 100;
+        }
+        return ((part/all)*100).toFixed(0);
+    };
+
     $scope.getDashboardData = function() {
         $http({
             method: 'POST',
