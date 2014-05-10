c8e82d33001751cf306aee8e0b390a515734d08a
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2014-05-10 18:33:50 +0400

    Поправил флаги для сборки

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -3,7 +3,7 @@ mkdir build && cd build
 wget http://projects.unbit.it/downloads/uwsgi-2.0.4.tar.gz && tar -xzf uwsgi-2.0.4.tar.gz
 cd uwsgi-2.0.4
 python2 uwsgiconfig.py --build
-printf "NAME='emperor_zeromq'\nCFLAGS = ['-lzmq']\nLDFLAGS = []\nLIBS = []\nGCC_LIST = ['emperor_zeromq']\n" > plugins/emperor_zeromq/uwsgiplugin.py
+printf "NAME='emperor_zeromq'\nCFLAGS = ['-lzmq']\nLDFLAGS = ['-lzmq']\nLIBS = []\nGCC_LIST = ['emperor_zeromq']\n" > plugins/emperor_zeromq/uwsgiplugin.py
 python2 uwsgiconfig.py --plugin plugins/emperor_zeromq
 cp uwsgi emperor_zeromq_plugin.so ../../
 cd ../..
