87625a9fe44cf6aa237dd38f12fc566254b58e9c
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2014-08-29 13:08:45 +0400

    Новая версия uwsgi

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build && cd build
-wget http://projects.unbit.it/downloads/uwsgi-2.0.4.tar.gz && tar -xzf uwsgi-2.0.4.tar.gz
-cd uwsgi-2.0.4
+wget http://projects.unbit.it/downloads/uwsgi-2.0.6.tar.gz && tar -xzf uwsgi-2.0.6.tar.gz
+cd uwsgi-2.0.6
 python2 uwsgiconfig.py --build
 printf "NAME='emperor_zeromq'\nCFLAGS = ['-lzmq']\nLDFLAGS = ['-lzmq']\nLIBS = []\nGCC_LIST = ['emperor_zeromq']\n" > plugins/emperor_zeromq/uwsgiplugin.py
 python2 uwsgiconfig.py --plugin plugins/emperor_zeromq
