6e587f99bb38261ec47184dffcd02ba5aeeb94eb
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2014-12-03 19:09:50 +0400

    Новая версия uwsgi

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build && cd build
-wget http://projects.unbit.it/downloads/uwsgi-2.0.6.tar.gz && tar -xzf uwsgi-2.0.6.tar.gz
-cd uwsgi-2.0.6
+wget http://projects.unbit.it/downloads/uwsgi-2.0.8.tar.gz && tar -xzf uwsgi-2.0.8.tar.gz
+cd uwsgi-2.0.8
 cp ../../forest.ini buildconf
 python2 uwsgiconfig.py --build forest
 python2 uwsgiconfig.py --plugin plugins/python forest python2
