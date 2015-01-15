186800f6062fdd7d38d87df171d9ce74e1ca3886
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-01-15 20:15:52 +0400

    API брокера для работы с листом

--- components/druid/__init__.py
+++ components/druid/__init__.py
@@ -1,11 +1,12 @@
 #coding=utf-8
 
-from components.druid.handlers import LeafHandler
+from components.druid.handlers import LeafHandler, LeavesHandler
 from components.druid.object import Druid
 
 
 druid_handlers = [
-    (r"/api/druid/leaf", LeafHandler),
+    (r"/api/druid/leaf", LeavesHandler),
+    (r'/api/druid/leaf/([\w\d]+)', LeafHandler)
 ]
 
 __all__ = ["Druid"]
--- components/druid/handlers.py
+++ components/druid/handlers.py
@@ -6,15 +6,13 @@ from tornado import gen
 import simplejson as json
 
 from components.api.handler import Handler
-from components.common import CustomEncoder, send_post_request, Message
+from components.common import send_post_request, send_request
 from bson import ObjectId
 
-from tornado.ioloop import IOLoop
-import time
 import random
 
 
-class LeafHandler(Handler):
+class LeavesHandler(Handler):
     @gen.coroutine
     def post(self):
         """
@@ -38,13 +36,9 @@ class LeafHandler(Handler):
 
         self.note("Looking up specie settings")
         try:
-            query = {
-                "_id": ObjectId(data["type"])
-            }
+            query = {"_id": ObjectId(data["type"])}
         except:
-            query = {
-                "name": data["type"]
-            }
+            query = {"name": data["type"]}
 
         species = yield self.application.async_db.species.find_one(query)
 
@@ -67,51 +61,100 @@ class LeafHandler(Handler):
             }}
         )
 
-        with Message(self, "Asking air to enable host..."):
-            for air in self.application.druid.air:
-                yield send_post_request(air, "air/hosts", {"host": data["address"]})
+        self.note("Asking air to enable host...")
+        for air in self.application.druid.air:
+            yield send_post_request(air, "air/hosts", {"host": data["address"]})
 
         if species.get("requires", []):
             self.note("Species {} requires following batteries: {}".format(
                 species["name"],
                 ", ".join(species["requires"]))
             )
+            self.note("Asking roots to prepare databases...")
 
-            with Message(self, "Asking roots to prepare databases..."):
-                roots = self.application.druid.roots[0]
-                self.note("Using roots server at {}".format(roots["host"]))
-                credentials = yield send_post_request(roots, "roots/db", {
-                    "name": data["name"],
-                    "db_type": species["requires"]
-                })
-                db_settings = credentials["data"]
+            roots = self.application.druid.roots[0]
+            self.note("Using roots server at {}".format(roots["host"]))
+            credentials = yield send_post_request(roots, "roots/db", {
+                "name": data["name"],
+                "db_type": species["requires"]
+            })
+            db_settings = credentials["data"]
 
-                yield self.application.async_db.leaves.update(
-                    {"_id": leaf_id},
-                    {"$set": {
-                        "batteries": db_settings
-                    }}
-                )
+            yield self.application.async_db.leaves.update(
+                {"_id": leaf_id},
+                {"$set": {
+                    "batteries": db_settings
+                }}
+            )
 
-                response = "\n"
-                for key, value in db_settings.items():
-                    response += "Settings for {}\n".format(key)
-                    response += "    User: {}\n".format(value["user"])
-                    response += "    Pass: {}\n".format(value["pass"])
-                    response += "    Name: {}\n".format(value["name"])
-                self.note(response)
+            response = "\n"
+            for key, value in db_settings.items():
+                response += "Settings for {}\n".format(key)
+                response += "    User: {}\n".format(value["user"])
+                response += "    Pass: {}\n".format(value["pass"])
+                response += "    Name: {}\n".format(value["name"])
+            self.note(response)
         else:
             db_settings = {}
 
-        with Message(self, "Asking branch to host leaf"):
-            branch = random.choice(self.application.druid.branch)
-            self.note("Randomly chosen branch server at {}".format(branch["host"]))
+        self.note("Asking branch to host leaf")
+        branch = random.choice(self.application.druid.branch)
+        self.note("Randomly chosen branch server at {}".format(branch["host"]))
 
-            yield send_post_request(roots, "branch/leaves", {
-                "_id": leaf_id
-            })
+        yield self.application.async_db.leaves.update(
+            {"_id": leaf_id},
+            {"$set": {"branch": branch["name"]}}
+        )
 
-        self.write("{}")
-        self.flush()
-        yield gen.Task(IOLoop.instance().add_timeout, time.time() + 5)
-        self.finish(json.dumps({}, cls=CustomEncoder))
+        leaf_config = yield self.application.async_db.leaves.find_one({"_id": leaf_id})
+        leaf_config["fastrouters"] = ["{host}:{fastrouter}".format(**a) for a in self.application.druid.air]
+
+        result = yield send_post_request(branch, "branch/leaves", leaf_config)
+        if result["data"]["result"] == "started":
+            self.note("Successfully started leaf")
+        elif result["data"]["result"] == "queued":
+            self.note("Leaf queued")
+        else:
+            self.note("Leaf start failed")
+        self.finish()
+
+
+class LeafHandler(Handler):
+    @gen.coroutine
+    def patch(self, leaf_name):
+        # Обрабатываем только ключи active, address
+        keys = ["active", "address"]
+        data = json.loads(self.request.body)
+
+        for key in data.keys():
+            if key not in keys:
+                del data[key]
+        leaf_data = yield self.application.async_db.leaves.find_one({"name": leaf_name})
+        if not leaf_data:
+            self.note("Unknown leaf specified")
+            self.finish()
+            raise gen.Return()
+
+        yield self.application.async_db.leaves.update(
+            {"name": leaf_name},
+            {"$set": data}
+        )
+
+        if data["active"] != leaf_data["active"]:
+            leaf_data_new = yield self.application.async_db.leaves.find_one({"name": leaf_name})
+            branch = next(x for x in self.application.druid.branch if x["name"] == leaf_data["branch"])
+
+            if data["active"]:
+                self.note("Starting leaf {}".format(leaf_name))
+                leaf_data_new["fastrouters"] = ["{host}:{fastrouter}".format(**a) for a in self.application.druid.air]
+                result = yield send_post_request(branch, "branch/leaves", leaf_data_new)
+                if result["data"]["result"] == "started":
+                    self.note("Successfully started leaf")
+                elif result["data"]["result"] == "queued":
+                    self.note("Leaf queued")
+                else:
+                    self.note("Leaf start failed")
+            else:
+                self.note("Stopping leaf {}".format(leaf_name))
+                yield send_request(branch, "branch/leaf/{}".format(str(leaf_data["_id"])), "DELETE")
+        self.finish()
