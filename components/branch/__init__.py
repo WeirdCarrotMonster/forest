20ccd7a9cbaf2789aff5c03068d3a70f4fbf4c46
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-02-10 00:37:55 +0400

    Микрофикс

--- components/branch/__init__.py
+++ components/branch/__init__.py
@@ -8,7 +8,7 @@ branch_handlers = [
     (r"/api/branch/leaf$", LeavesHandler),
     (r"/api/branch/leaf/([0-9a-fA-F]{24})$", LeafHandler),
     (r"/api/branch/species$", SpeciesListHandler),
-    (r"/api/branch/species/([0-9a-fA-F]{24})$", SpeciesHandler)
+    (r"/api/branch/species/([0-9a-fA-F]{24})$", SpeciesHandler),
     (r"/api/branch/loggers$", LoggerListHandler),
     (r"/api/branch/loggers/([0-9a-fA-F]{*})$", LoggerHandler)
 ]
