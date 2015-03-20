99e929a70f88880a52bed17e5c309075bc752e0b
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-03-20 16:08:49 +0400

    Выкинул нахуй создаваемые базы. Только shared, только хардкор.

--- forest/components/roots/batteries.py
+++ forest/components/roots/batteries.py
@@ -4,26 +4,19 @@ from __future__ import unicode_literals, print_function
 
 import random
 import string
-import os
-import time
-import subprocess
 
-from tornado.gen import coroutine, Return, Task
-from tornado.ioloop import IOLoop
+from tornado.gen import coroutine
 import pymysql
 import motor
-from pymysql import OperationalError
 import traceback
 
 from forest.components.emperor import Vassal
-from forest.components.common import log_message
-from forest.components.cmdrunner import call_subprocess
 
 
 # pylint: disable=W0612,W0221,W0702
 
 
-class Battery(Vassal):
+class Battery(object):
 
     def __init__(
             self,
@@ -34,8 +27,7 @@ class Battery(Vassal):
             username=None,
             password=None,
             database=None,
-            **kwargs):
-        super(Battery, self).__init__(**kwargs)
+            ):
         self.__path__ = path
         self.__port__ = port
         self.__owner__ = owner
@@ -68,142 +60,8 @@ class Battery(Vassal):
             "database": self.__database__
         }
 
-    @property
-    def config_ext(self):
-        raise NotImplementedError
-
-    @coroutine
-    def wait(self):
-        raise NotImplementedError
-
-
-class MySQL(Battery):
-
-    @property
-    def config_ext(self):
-        return "mysql"
-
-    @coroutine
-    def initialize(self):
-        if not os.path.exists(self.__path__):
-            log_message("Initializing database directory")
-            os.makedirs(self.__path__)
-            result, error = yield call_subprocess(
-                [
-                    "mysql_install_db",
-                    "--no-defaults",
-                    "--datadir={}".format(self.__path__),
-                    "--basedir=/usr"
-                ]
-            )
-
-            with open(os.path.join(self.__path__, "firstrun.sql"), "w") as config:
-                config.write("""
-DELETE FROM mysql.user;
-CREATE USER 'root'@'%' IDENTIFIED BY '{0}';
-GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
-DROP DATABASE IF EXISTS test;
-CREATE DATABASE IF NOT EXISTS `{1}` CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON {1}.* TO '{2}'@'%' IDENTIFIED BY '{3}' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-""".format(self.__rootpass__, self.__database__, self.__username__, self.__password__)
-                )
-        else:
-            pass
-
-    @coroutine
-    def wait(self, timeout=30):
-        t = timeout
-        while t >= 0:
-            t -= 1
-
-            try:
-                pymysql.connect(
-                    host="127.0.0.1",
-                    port=self.__port__
-                )
-                raise Return(True)
-            except OperationalError as e:
-                if "Access denied for user" in e.args[1]:
-                    raise Return(True)
-
-            yield Task(IOLoop.current().add_timeout, time.time() + 1)
-
-        raise Return(False)
-
-    def __get_config__(self):
-        return """[uwsgi]
-master=true
-attach-daemon=mysqld --no-defaults --datadir={0} --port={1} --socket={0}/socket --skip-name-resolve --init-file={0}/firstrun.sql
-""".format(self.__path__, self.__port__)
-
 
 class Mongo(Battery):
-
-    @property
-    def config_ext(self):
-        return "mongo"
-
-    @coroutine
-    def initialize(self):
-        if not os.path.exists(self.__path__):
-            log_message("Creating directory for {}".format(self.__owner__), component="Roots")
-            os.makedirs(self.__path__)
-
-            proc = subprocess.Popen([
-                "mongod",
-                "--dbpath={}".format(self.__path__),
-                "--port={}".format(self.__port__),
-                "--bind_ip=127.0.0.1",
-                "--noauth",
-                "--logpath={}".format(os.path.join(self.__path__, "log.txt")),
-                "--logappend"
-            ])
-
-            yield self.wait(auth=False)
-
-            client = motor.MotorClient("127.0.0.1", self.__port__)
-            yield client[self.__database__].add_user(
-                name=self.__username__,
-                password=self.__password__,
-                roles=["readWrite"]
-            )
-
-            proc.terminate()
-            proc.wait()
-
-    @coroutine
-    def wait(self, timeout=60, auth=True):
-        t = timeout
-        client = motor.MotorClient("mongodb://127.0.0.1:{}".format(self.__port__), connectTimeoutMS=500)
-
-        if auth:
-            client[self.__database__].authenticate(self.__username__, self.__password__)
-        while t >= 0:
-            t -= 1
-            try:
-                yield client[self.__database__].eval("help")
-                raise Return(True)
-            except motor.pymongo.errors.AutoReconnect:
-                pass
-            except motor.pymongo.errors.OperationFailure:
-                raise Return(True)
-            yield Task(IOLoop.current().add_timeout, time.time() + 2)
-
-        raise Return(False)
-
-    def __get_config__(self):
-        return """[uwsgi]
-master=true
-attach-daemon=mongod --dbpath={0} --port={1} --auth --logpath={2} --logappend
-""".format(self.__path__, self.__port__, os.path.join(self.__path__, "log.txt"))
-
-
-class MongoShared(Battery):
-
-    @property
-    def config_ext(self):
-        return "mongo_shared"
 
     @coroutine
     def initialize(self):
@@ -217,26 +75,12 @@ class MongoShared(Battery):
         db = client[str(self.__database__)]
         yield db.add_user(self.__username__, self.__password__, roles=["readWrite"])
 
-    @coroutine
-    def wait(self):
-        pass
 
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
-
-
-class MysqlShared(Battery):
+class MySQL(Battery):
 
     def __init__(self, *args, **kwargs):
-        super(MysqlShared, self).__init__(*args, **kwargs)
+        super(MySQL, self).__init__(*args, **kwargs)
         self.__username__ = self.__database__[len(self.__database__)-16:]
-
-    @property
-    def config_ext(self):
-        return "mysql_shared"
 
     @coroutine
     def initialize(self):
@@ -265,13 +109,3 @@ class MysqlShared(Battery):
             db.close()
         except:
             print(traceback.format_exc())
-
-    @coroutine
-    def wait(self):
-        pass
-
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
--- forest/components/roots/object.py
+++ forest/components/roots/object.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
 
-import os
-import sys
-from bson import json_util
-
 from tornado.gen import Return, coroutine
-import simplejson as json
 
-from forest.components.roots.batteries import MySQL, Mongo, MongoShared, MysqlShared
+from forest.components.roots.batteries import Mongo, MySQL
 from forest.components.common import log_message
 
 
@@ -23,112 +18,23 @@ class Roots(object):
         self.__mongo_settings__ = self.settings.get("mongo", {})
         self.__mysql_settings__ = self.settings.get("mysql", {})
 
-        self.__roots_dir__ = self.settings.get("roots_dir") or os.path.join(self.trunk.forest_root, "roots")
         log_message("Started roots", component="Roots")
 
-        self.batteries_mysql = {}
-        self.batteries_mongo = {}
-        self.port_range = set(range(30000, 35000))
-
-        self.initialize()
-
-    @property
-    def root(self):
-        return self.__roots_dir__
-
-    @property
-    def metaroot(self):
-        return os.path.join(self.root, "meta")
-
-    @property
-    def dataroot(self):
-        return os.path.join(self.root, "data")
-
-    def initialize(self):
-        if not self.__mysql_settings__.get("type", "standalone") == "standalone":
-            return
-
-        for f in os.listdir(self.metaroot):
-            if not os.path.isfile(os.path.join(self.metaroot, f)):
-                continue
-
-            name, ext = os.path.splitext(f)
-
-            with open(os.path.join(self.metaroot, f)) as config:
-                config = json.load(config, object_hook=json_util.object_hook)
-                if all(_ in config for _ in ["username", "password", "rootpass", "database", "path", "port", "owner"]):
-                    log_message("Malformed config for {}{}, skipping".format(name, ext))
-                    continue
-
-                if ext == ".mysql":
-                    log_message("Starting mysql server for {}".format(config["owner"]), component="Roots")
-
-                    self.port_range -= {config["port"]}
-
-                    self.batteries_mysql[config["owner"]] = MySQL(emperor=self.trunk.emperor, **config)
-                    self.batteries_mysql[config["owner"]].start()
-
-                if ext == ".mongo":
-                    log_message("Starting mongo server for {}".format(config["owner"]), component="Roots")
-
-                    self.port_range -= {config["port"]}
-
-                    self.batteries_mongo[config["owner"]] = Mongo(emperor=self.trunk.emperor, **config)
-                    self.batteries_mongo[config["owner"]].start()
-
-    def save_config(self, battery):
-        cfg_name = "{}.{}".format(battery.owner, battery.config_ext)
-
-        with open(os.path.join(self.metaroot, cfg_name), "w") as config:
-            config.write(
-                json.dumps(battery.config, default=json_util.default, indent=2)
-            )
-
-    def cleanup(self):
-        for key, value in self.batteries_mysql.items():
-            value.stop()
-        for key, value in self.batteries_mongo.items():
-            value.stop()
-
     def __get_mongo__(self, name):
-        mongo_type = self.__mongo_settings__.get("type", "standalone")
-        if mongo_type == "standalone":
-            return Mongo(
-                emperor=self.trunk.emperor,
-                owner=str(name),
-                port=self.port_range.pop(),
-                path=os.path.join(self.dataroot, "{}_mongo".format(name))
-            )
-        elif mongo_type == "shared":
-            return MongoShared(
-                owner=str(name),
-                port=27017,
-                rootpass=self.__mongo_settings__.get("rootpass", "password"),
-                database=name
-            )
-        else:
-            log_message("Unknown configuration specified for roots.mongo.type: {}".format(mongo_type))
-            sys.exit(0)
+        return Mongo(
+            owner=str(name),
+            port=27017,
+            rootpass=self.__mongo_settings__.get("rootpass", "password"),
+            database=str(name)
+        )
 
     def __get_mysql__(self, name):
-        mysql_type = self.__mysql_settings__.get("type", "standalone")
-        if mysql_type == "standalone":
-            return MySQL(
-                emperor=self.trunk.emperor,
-                owner=str(name),
-                port=self.port_range.pop(),
-                path=os.path.join(self.dataroot, "{}_mysql".format(name))
-            )
-        elif mysql_type == "shared":
-            return MysqlShared(
-                owner=str(name),
-                port=3306,
-                rootpass=self.__mysql_settings__.get("rootpass", "password"),
-                database=name
-            )
-        else:
-            log_message("Unknown configuration specified for roots.mysql.type: {}".format(mysql_type))
-            sys.exit(0)
+        return MySQL(
+            owner=str(name),
+            port=3306,
+            rootpass=self.__mysql_settings__.get("rootpass", "password"),
+            database=str(name)
+        )
 
     @coroutine
     def create_db(self, _id, db_type):
@@ -136,11 +42,7 @@ class Roots(object):
         if "mysql" in db_type:
             log_message("Creating mysql for {}".format(_id), component="Roots")
             db = self.__get_mysql__(_id)
-            self.batteries_mysql[_id] = db
             yield db.initialize()
-            db.start()
-            self.save_config(db)
-            yield db.wait()
             credentials["mysql"] = {
                 "host": self.trunk.host,
                 "port": db.__port__,
@@ -151,11 +53,7 @@ class Roots(object):
         if "mongo" in db_type:
             log_message("Creating mongodb for {}".format(_id), component="Roots")
             db = self.__get_mongo__(_id)
-            self.batteries_mongo[_id] = db
             yield db.initialize()
-            db.start()
-            self.save_config(db)
-            yield db.wait()
             credentials["mongo"] = {
                 "host": self.trunk.host,
                 "port": db.__port__,
