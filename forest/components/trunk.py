35848db159232ee4e4d81df1317dfe6ea0055ed3
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-03-31 00:26:20 +0400

    Чуток переименовал переменные для внятности

--- forest/components/trunk.py
+++ forest/components/trunk.py
@@ -12,16 +12,16 @@ from forest.components.database import get_default_database
 
 class Trunk(tornado.web.Application):
 
-    def __init__(self, settings_dict, **settings):
-        super(Trunk, self).__init__(**settings)
+    def __init__(self, settings, **kwargs):
+        super(Trunk, self).__init__(**kwargs)
         self.settings["cookie_secret"] = "asdasd"
-        self.name = settings_dict["name"]
-        self.root = settings_dict["root"]
-        self.host = settings_dict["host"]
-        self.secret = settings_dict["secret"]
-        self.emperor_dir = settings_dict.get("emperor", os.path.join(self.forest_root, "emperor"))
+        self.name = settings["name"]
+        self.root = settings["root"]
+        self.host = settings["host"]
+        self.secret = settings["secret"]
+        self.emperor_dir = settings.get("emperor", os.path.join(self.forest_root, "emperor"))
 
-        self.database = settings_dict.get("db")
+        self.database = settings.get("db")
         if self.database:
             self.async_db = get_default_database(self.database, async=True)
             self.sync_db = get_default_database(self.database)
