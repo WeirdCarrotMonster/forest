6a90d49dc36591b254f9e670a2b8d83be7b93596
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-03-31 00:20:13 +0400

    Мажу код комментами

--- forest/components/leaf.py
+++ forest/components/leaf.py
@@ -26,15 +26,19 @@ class Leaf(Vassal):
         self.__fastrouters__ = fastrouters or []
         self.__species__ = species
         self.__batteries__ = batteries
-        self.__log_port__ = None
+        self.__log_port__ = log_port
         self.settings = settings or {}
         self.address = address
         self.workers = workers
         self.threads = threads
-        self.log_port = log_port
         self.leaf_host = leaf_host
 
     def start(self):
+        """Запускает лист
+
+        Перед непосредственным запуском метод выполняет проверку текущего состояния вида - если он готов, то выполняется
+        непосредственный запуск листа, иначе лист изменяет состояние на "Queued" и встает в очередь ожидания
+        """
         if self.__species__.is_ready:
             super(Leaf, self).start()
             return True
@@ -43,18 +47,26 @@ class Leaf(Vassal):
 
     @property
     def keyfile(self):
+        """Используемый при аутентификации с fastrouter'ом файл ключа
+        :returns: Полный путь к файлу ключа
+        :rtype: str
+        """
         return self.__keyfile__
 
     @property
     def log_port(self):
+        """Локальный порт, на который будут отправляться все логи событий
+        :returns: Номер порта логов
+        :rtype: int
+        """
         return self.__log_port__
-
-    @log_port.setter
-    def log_port(self, value):
-        self.__log_port__ = value
 
     @property
     def dict(self):
+        """Словарь с конфигурацией листа, по которой можно провести его создание
+        :returns: Словарь со значением всех полей экземпляра листа
+        :rtype: dict
+        """
         data = super(Leaf, self).dict
         data.update({
             "settings": self.settings,
@@ -69,39 +81,60 @@ class Leaf(Vassal):
 
     @property
     def species(self):
-        """Возвращает тип листа
-        :rtype : Species
-        :return:
+        """Используемый тип листа
+        :returns: Экземпляр типа листа
+        :rtype: Species
         """
         return self.__species__
 
     @species.setter
     def species(self, value):
+        """Устанавливает используемый тип листа
+        :param value: Экземпляр типа листа
+        :type value: Species
+        """
         self.__species__ = value
 
-    @property
-    def environment(self):
-        return {
-            "BATTERIES": dumps(self.__batteries__),
-            "APPLICATION_SETTINGS": dumps(self.settings)
-        }
-
     def pause(self):
+        """Приостанавливает работу листа
+
+        Приостановление работы отличается от полной остановки тем, что в отличии от остановки не удаляет
+        конфигурационный файл полностью, а заменяет его на используемый для отображения заглушки
+        """
         self.status = "Paused"
         # TODO: Раскомментировать, когда будет работать приостановленный конфиг
         # self.__emperor__.start_vassal(self)
         self.__emperor__.stop_vassal(self)
 
     def get_config(self):
+        """Переопределенный метод получения конфигурации uwsgi-вассала
+
+        Метод get_config листа отличается от оригинального тем, что возвращает разную конфигурацию в зависимости от
+        текущего статуса: обычную, в случае, если лист запущен и заглушку, если лист остановлен.
+        :returns: Конфигурация uwsgi-вассала
+        :rtype: str
+        """
         if self.status != "Paused":
             return self.__get_config__()
         else:
             return self.__get_config_paused__()
 
     def __get_config_paused__(self):
+        """Конфигурация листа в приостановленном состоянии
+        :returns: Конфигурация uwsgi-вассала
+        :rtype: str
+
+        .. todo::
+            Описать конфигурацию для приложения-заглушки, которое будет передавать только страницу с сообщением о
+            проведении технических работ
+        """
         return """[uwsgi]"""
 
     def __get_config__(self):
+        """Конфигурация листа в нормальном состоянии
+        :returns: Конфигурация uwsgi-вассала
+        :rtype: str
+        """
         logs_format = {
             "uri": "%(uri)",
             "addr": "%(addr)",
@@ -167,7 +200,7 @@ endif=
             python=self.__species__.python,
             threads="enable-threads=" if self.threads else "",
             triggers=self.get_triggers_config(),
-            virtualenv=self.__species__.environment,
+            virtualenv=self.species.environment,
             workers=self.workers
         )
 
