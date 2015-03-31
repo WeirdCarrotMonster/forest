617f84d5a0d44284e63eff07bb393d5c71963b39
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-03-31 16:27:24 +0400

    Минус неиспользуемый код

--- forest/components/api/handler.py
+++ forest/components/api/handler.py
@@ -10,13 +10,7 @@ class Handler(web.RequestHandler):
     def __init__(self, application, request, **kwargs):
         super(Handler, self).__init__(application, request, **kwargs)
         self.user = self.get_secure_cookie("user")
-        self.interactive = self.request.headers.get("Interactive", "False") == "True"
         self.closed = False
-
-    def note(self, note):
-        if self.interactive:
-            self.write(note)
-            self.flush()
 
     def on_connection_close(self):
         self.closed = True
