2685cefd8a7b232a359562f18a5a171dc98b74dd
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-04-01 14:36:55 +0400

    Схема для POST /api/druid/leaves

---
+++ forest/jsonschema/druid.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+
+leaves = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "id": "",
+    "type": "object",
+    "properties": {
+        "name": {
+          "id": "/name",
+          "type": "string"
+        },
+        "type": {
+            "id": "/type",
+            "type": "string"
+        },
+        "address": {
+            "id": "/address",
+            "type": "string"
+        }
+    },
+    "required": [
+        "name",
+        "type",
+        "address"
+    ]
+}
